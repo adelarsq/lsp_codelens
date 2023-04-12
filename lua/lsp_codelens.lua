2ffe2dfb796a6167fc65eb75a1e433d58c61f913
Adelar da Silva Queiróz <adelarsq@gmail.com>
2023-04-12 18:27:51 -0300

    Initial commit

---
+++ lua/lsp_codelens.lua
@@ -0,0 +1,59 @@
+local M = {}
+
+-- Create one augroup per buffer
+-- Based on https://www.reddit.com/r/neovim/comments/u4g6jh/comment/i4vrkqk/?utm_source=share&utm_medium=web2x&context=3
+local augroup = function(group, augroup_opts)
+    return setmetatable({
+        id = vim.api.nvim_create_augroup(
+            group,
+            vim.tbl_extend('keep', augroup_opts or {}, { clear = true })
+        ),
+        del = function(self)
+            if self.id then
+                vim.api.nvim_del_augroup_by_id(self.id)
+                self.id = nil
+            end
+        end,
+    }, {
+        __call = function(self, callback)
+            if self.id then
+                callback(function(event, create_opts)
+                    create_opts.group = self.id
+                    vim.api.nvim_create_autocmd(event, create_opts)
+                end, function(clear_opts)
+                    clear_opts.group = self.id
+                    vim.api.nvim_clear_autocmds(clear_opts)
+                end)
+            end
+        end,
+    })
+end
+
+function M.on_attach(client, bufnr)
+    local MY_LSP_AUGROUP = augroup('MY_LSP_AUGROUP', {})
+    MY_LSP_AUGROUP(function(autocmd, clear)
+        clear { buffer = bufnr }
+
+        autocmd({ 'BufEnter', 'CursorHold', 'InsertLeave' }, {
+            buffer = bufnr,
+            callback = function()
+                if client.server_capabilities.codeLensProvider then
+                    vim.lsp.codelens.refresh()
+                end
+
+                if client.server_capabilities.document_highlight then
+                    vim.lsp.buf.document_highlight()
+                end
+            end,
+        })
+
+        autocmd('LspDetach', {
+            buffer = bufnr,
+            callback = function(opt)
+                vim.lsp.codelens.clear(opt.data.client_id, opt.buf)
+            end
+        })
+    end)
+end
+
+return M
